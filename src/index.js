bc0d08243f0b2dd2a303bc958358230a91366a5b
Antony Sanders <antony.devon92@gmail.com>
2020-05-30 07:07:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,5 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function fetchImages() {
-  
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl).then(resp => resp.json())
+  .then(json => renderImage(json.message));
 }
+
+
